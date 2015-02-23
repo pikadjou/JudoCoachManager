f00feb393efa48592b71284a99f52e8a403b7123
Geoffrey Lambert <geoffrey.lambert@produweb.be>
2015-02-23 13:55:27 +0100

    set example for angular

--- JudoCoachManager/scripts/index.ts
+++ JudoCoachManager/scripts/index.ts
@@ -32,3 +32,109 @@ module JudoCoachManager {
         Application.initialize();
     }
 }
+
+var app = angular.module('judoapp', ['ngRoute', 'ionic', 'elif', 'angular-datepicker']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('app', {
+            url: "/app",
+            abstract: true,
+            templateUrl: 'menu.html'
+        })
+        .state('app.index', {
+            url: "/index",
+            views: {
+                'menuContent': {
+                    templateUrl: 'partials/index.html'
+                }
+            }
+        })
+        .state('app.home', {
+            url: "/home",
+            views: {
+                'menuContent': {
+                    templateUrl: 'partials/home.html'
+                },
+                'leftContent': {
+                    templateUrl: 'partials/side/management.html'
+                }
+            }
+        })
+        .state('app.competition', {
+            url: "/competition/:id",
+            views: {
+                'menuContent': {
+                    templateUrl: 'partials/competition.html'
+                },
+                'leftContent': {
+                    templateUrl: 'partials/side/management.html'
+                }
+            }
+        })
+        .state('app.category', {
+            url: "/category/:id",
+            views: {
+                'menuContent': {
+                    templateUrl: 'partials/category.html'
+                },
+                'leftContent': {
+                    templateUrl: 'partials/side/management.html'
+                },
+                'rightContent': {
+                    templateUrl: 'partials/side/management.html'
+                }
+            }
+        })
+        .state('app.encodeTab', {
+            url: "/encodeTab/:id",
+            views: {
+                'menuContent': {
+                    templateUrl: 'partials/encodeTab.html'
+                },
+                'rightContent': {
+                    templateUrl: 'partials/side/management.html'
+                },
+                'leftContent': {
+                    templateUrl: 'partials/side/management.html'
+                }
+
+            }
+        })
+        .state('app.tab', {
+            url: "/tab/:id",
+            views: {
+                'menuContent': {
+                    templateUrl: 'partials/tab.html'
+                },
+                'leftContent': {
+                    templateUrl: 'partials/side/management.html'
+                },
+                'rightContent': {
+                    templateUrl: 'partials/side/management.html'
+                }
+            }
+        })
+        .state('app.poule', {
+            url: "/poule/:id",
+            views: {
+                'menuContent': {
+                    templateUrl: 'partials/poule.html'
+                },
+                'leftContent': {
+                    templateUrl: 'partials/side/management.html'
+                }
+            }
+        })
+        .state('app.fight', {
+            url: "/fight/:id",
+            views: {
+                'menuContent': {
+                    templateUrl: 'partials/fight.html'
+                },
+                'rightContent': {
+                    templateUrl: 'partials/side/management.html'
+                }
+            }
+        });
+    $urlRouterProvider.otherwise('/app/index');
