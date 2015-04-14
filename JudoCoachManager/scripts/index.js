f867f63550dae93e6bba7288a8dd8e08f5c87f18
Geoffrey Lambert <geoffrey.lambert@produweb.be>
2015-04-14 19:30:25 +0200

    config db

--- JudoCoachManager/scripts/index.js
+++ JudoCoachManager/scripts/index.js
@@ -2,7 +2,7 @@
 var JudoCoachManager;
 (function (JudoCoachManager) {
     "use strict";
-
+    var db = null;
     var coachapp = angular.module('coachapp', ['ngRoute', 'ionic', 'elif', 'angular-datepicker']).controller('HomeCtrl', JudoCoachManager.HomeCtrl).config([
         "$stateProvider", "$urlRouterProvider",
         function ($stateProvider, $urlRouterProvider) {
@@ -119,6 +119,7 @@ var JudoCoachManager;
     (function (Application) {
         function initialize() {
             document.addEventListener('deviceready', onDeviceReady, false);
+            onDeviceReady();
         }
         Application.initialize = initialize;
 
@@ -126,7 +127,61 @@ var JudoCoachManager;
             // Handle the Cordova pause and resume events
             document.addEventListener('pause', onPause, false);
             document.addEventListener('resume', onResume, false);
+
             // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+            db = window.openDatabase("Database", "1.0", "JudoCoachManager", 200000);
+            db.transaction(populateDB, errorCB, successCB);
+        }
+
+        // Alimentation de la base de données
+        //
+        function populateDB(tx) {
+            //    tx.executeSql('DROP TABLE IF EXISTS COMPETITIONS');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS COMPETITIONS (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, created TEXT);');
+
+            //   tx.executeSql('DROP TABLE IF EXISTS CATEGORIES_COMPETITIONS');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS CATEGORIES_COMPETITIONS (id INTEGER PRIMARY KEY AUTOINCREMENT, competition_id INTEGER, category_id INTEGER, number INTEGER);');
+
+            //    tx.executeSql('DROP TABLE IF EXISTS JUDOKAS_COMPETITIONS');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS JUDOKAS_COMPETITIONS (id INTEGER PRIMARY KEY AUTOINCREMENT, category_competition_id INTEGER, judoka_id INTEGER, place INTEGER, number INTEGER, fights INTEGER, mine INTEGER, weigh TEXT);');
+
+            //    tx.executeSql('DROP TABLE IF EXISTS FIGHTS');
+            tx.executeSql("CREATE TABLE IF NOT EXISTS FIGHTS (id INTEGER PRIMARY KEY AUTOINCREMENT, category_competition_id INTEGER, type INTEGER, number INTEGER, white_id INTEGER, blue_id INTEGER, score_white INTEGER, score_blue INTEGER, winner INTEGER);");
+
+            //   tx.executeSql('DROP TABLE IF EXISTS JUDOKAS');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS JUDOKAS (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, club TEXT, mine INTEGER);');
+
+            //    tx.executeSql('DROP TABLE IF EXISTS CATEGORIES');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS CATEGORIES (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);');
+        }
+
+        // Fonction de callback en cas d'échec de la transaction
+        //
+        function errorCB(err) {
+            console.log("Erreur de traitement SQL (Init) : " + err);
+            console.log(err);
+        }
+
+        // Fonction de callback en cas de réussite de la transaction
+        //
+        function successCB() {
+            console.log("cool ma poule");
+            db.transaction(function (tx) {
+                var d = new Date();
+                tx.executeSql("INSERT INTO COMPETITIONS(name,created) values('test1','')");
+            });
+            //    $db.transaction(function(tx){
+            //        tx.executeSql("SELECT * FROM COMPETITIONS JOIN CATEGORIES;",[],
+            //            function(tx, results){
+            //                var data = [];
+            //                for (var i=0; i<results.rows.length; i++){
+            //                    data[i] = results.rows.item(i);
+            //                }
+            //                console.log(data);
+            //            }, function (tx, err) { alert("Erreur de traitement SQL : "+err.message); },
+            //            function (tx, err) { alert("Erreur de traitement SQL : "+err.message); }
+            //        );
+            //    });
         }
 
         function onPause() {
